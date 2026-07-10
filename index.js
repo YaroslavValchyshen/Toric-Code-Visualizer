2390f2117adfadac03cc7203f8dad138699f3c42
YaroslavValchyshen <51487067+YaroslavValchyshen@users.noreply.github.com>
2026-07-10 13:36:57 +0000

    Deploying to gh-pages from @ YaroslavValchyshen/Toric-Code-Visualizer@834cc1d46978f0e275025cd11391e88c705b201a 🚀

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpblo581ik.js
+// include: /tmp/tmpkdj3hko9.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -195,25 +195,25 @@ Module['FS_createPath']("/", "Shaders", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/Shaders/fragment.frag", "start": 0, "end": 98}, {"filename": "/Shaders/vertex.vert", "start": 98, "end": 216}], "remote_package_size": 216});
+    loadPackage({"files": [{"filename": "/Shaders/fragment.frag", "start": 0, "end": 94}, {"filename": "/Shaders/vertex.vert", "start": 94, "end": 212}], "remote_package_size": 212});
 
   })();
 
-// end include: /tmp/tmpblo581ik.js
-// include: /tmp/tmpj7sh8d9n.js
+// end include: /tmp/tmpkdj3hko9.js
+// include: /tmp/tmpwpm6j91h.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpj7sh8d9n.js
-// include: /tmp/tmpox0cno7d.js
+  // end include: /tmp/tmpwpm6j91h.js
+// include: /tmp/tmpr1j9s2x3.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpox0cno7d.js
+  // end include: /tmp/tmpr1j9s2x3.js
 
 
 var programArgs = [];
